9d44aaa20c2457b3d5de422467a93043fb0a8a0c
gf38 <gf38@st-andrews.ac.uk>
2015-04-11 20:25:15 +0100

    Added SoundFileReader

---
+++ src/SoundFileReader.java
@@ -0,0 +1,85 @@
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.sound.sampled.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Gergely
+ */
+public class SoundFileReader {
+
+    AudioInputStream inputStream;
+    AudioFormat audioFormat;
+    byte[] buffer;
+    int frameCounter;            
+
+    public long getFileSizeInBytes() {
+        return inputStream.getFrameLength() * audioFormat.getFrameSize();
+    }
+
+    public long getFrameLength() {
+        return inputStream.getFrameLength();
+    }
+
+    public long getFrameSize() {
+        return audioFormat.getFrameSize();
+    }
+
+    public SoundFileReader(String filePath) throws UnsupportedAudioFileException, IOException {
+
+        File file = new File(filePath);
+
+        if (file.canRead()) {
+            inputStream = AudioSystem.getAudioInputStream(file);
+            audioFormat = inputStream.getFormat();
+            buffer = new byte[audioFormat.getFrameSize()];
+        } else {
+            throw new IOException("Can't read file!");
+        }
+    }
+
+    public AudioFormat getAudioFormat() {
+        return audioFormat;
+    }
+
+    /**
+     * Reads one frame, and returns the entire frame, or returns null of there
+     * is nothing to be read.
+     *
+     * @return byte array containing the contents of the frame
+     * @throws IOException
+     */
+    public byte[] readFrame() throws IOException {
+        int bytesRead = inputStream.read(buffer);
+
+        if (bytesRead != -1) {
+            return buffer;
+        } else {
+            return null;
+        }
+    }
+    
+    public byte readByte() throws IOException{
+        if(frameCounter == audioFormat.getFrameSize()){
+            frameCounter = 0;
+            byte[] check = readFrame();
+            if(check == null){
+                throw new EOFException("End of audio file reached!");
+            }
+            
+        }
+        else{
+            frameCounter++;     
+        }
+        
+        return buffer[frameCounter];
+    }
+}
